96c229b01f2d25a81afe71560b0abed578e6adeb
nevinnnnnnn <nevinsunil2003@gmail.com>
2025-07-25 17:09:13 +0530

    Initial commit

---
+++ .ipynb_checkpoints/JULY23-checkpoint.py
@@ -0,0 +1,20 @@
+#all the question of this file is taken from chatgpt with an approproate prompt given to it.
+
+#Q1. Add a single-line comment that says "This program prints Hello World".
+"""this program prints 
+hello world
+in multiple lines"""
+
+
+"""Q2. Declare variables to store the following:
+Your name (text)
+Your age (number)
+Whether you're a student (True/False)
+Your height in meters (decimal)"""
+
+name = "nevin"
+age = 21
+student = True
+height = 168.7
+
+#Q3 define the type of the above variables
