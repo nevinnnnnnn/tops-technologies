14a4a525e42ccf298fbe63364fe59aa40af5e625
nevinnnnnnn <nevinsunil2003@gmail.com>
2025-07-30 15:03:28 +0530

    Saving unstaged changes before rebase

--- Assignment/LabPractice/movieTicketPricing.py
+++ Assignment/LabPractice/movieTicketPricing.py
@@ -1,9 +1,9 @@
 age = int(input("enter the age: "))
-if age < 5 or age > 60:
+if age < 5 and age > 60:
     print("ticket is free")
-elif age <=18 or age >=5:
+elif age <=18 and age >=5:
     print("price for ticket is 100 rupee")
-elif age <= 60 or age >= 19:
+elif age <= 60 and age >= 19:
     print("price for ticket is 200 rupee")
 else:
     print("age is invalid")
