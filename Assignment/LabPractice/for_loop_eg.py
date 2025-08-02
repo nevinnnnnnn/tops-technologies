927915fe0be4a470f3be20f6d3d2e15047033604
nevinnnnnnn <nevinsunil2003@gmail.com>
2025-08-02 13:28:16 +0530

    added notes given by the mentor

---
+++ Assignment/LabPractice/for_loop_eg.py
@@ -0,0 +1,26 @@
+""" cart=["bottle","maggie","cheeseeeeee","kit-kat","cloth"]
+for i in cart:
+    print(f"my item is  {i}")   """
+
+"""for i in range(3,9):
+    print(f"my range {i}")"""
+
+
+even_num=[]
+odd_num=[]
+for i in  range(1,90):
+    if i%2==0:
+        even_num.append(i)
+    else:
+        odd_num.append(i)
+
+print("even number:",even_num)
+print("odd numbers:", odd_num)
+          
+"""
+name = ["harshil", "karan","nevin","avni","dev"]
+for repetation in name:
+    if repetation == "karan":
+        print(f"gift nahi dugaa {repetation}")
+    else:
+        print(f"gift dugaa {repetation}") """
