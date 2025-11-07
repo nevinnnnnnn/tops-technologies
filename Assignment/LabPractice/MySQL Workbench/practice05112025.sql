447b8288ad267438bda9b113e8b47a71389e0a63
nevin <nevinsunil2003@gmail.com>
2025-11-07 12:45:51 +0530

    added the assessment

--- Assignment/LabPractice/MySQL Workbench/practice05112025.sql
+++ Assignment/LabPractice/MySQL Workbench/practice05112025.sql
@@ -18,11 +18,21 @@ select * from orders where total_amount < 200 and payment_status = "paid";
 -- aggregation function - count, sum, average, min, max
 
 
-
 select order_status, sum(total_amount) as total_amount from orders group by order_status;
 
 
 -- store with atleast 3 delivered orders
--- customers hwose highest orders above 200
+show tables;
+select * from orders where order_status = "Delivered" limit 3;
+
+-- customers hwose highest orders above 300
+show tables;
+select * from customer, orders where total_amount > 300;
+
+-- select customer_id from orders and cust_name from customer where total_amount > 200;
+select customer_id, cust_name from customer where customer_id in (select customer_id from orders where total_amount > 200);
+
 -- store where avg orders amount is between 100 and 400 (asc and desc for ascending and descending respectively) (limit is for getting only the particular values) (offset is the gap)
--- 
+select * from products;
+
+select product_name, price from products where price between 21 and 100;
